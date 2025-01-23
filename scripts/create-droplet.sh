b9fd43d6a7ee1a28c8438735f6e14cc8d0f8547f
Tim Child <timjchild@gmail.com>
2025-01-23 09:31:27 -0800

    add monitoring to droplets

--- scripts/create-droplet.sh
+++ scripts/create-droplet.sh
@@ -16,7 +16,7 @@ if [ -z "$ssh_key_id" ]; then
 fi
 
 # Create a new droplet
-doctl compute droplet create $droplet_name --image ubuntu-24-04-x64 --region sfo3 --size s-1vcpu-512mb-10gb --ssh-keys $ssh_key_id --wait
+doctl compute droplet create $droplet_name --image ubuntu-24-04-x64 --region sfo3 --size s-1vcpu-512mb-10gb --ssh-keys $ssh_key_id --enable-monitoring --wait
 
 echo "Droplet created successfully!"
 
